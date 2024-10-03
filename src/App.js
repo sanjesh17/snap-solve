c0414473a2ac05367c9f1d0c479336cf5c3075b0
sriram <sriramoholic254@gmail.com>
2024-10-03 17:23:46 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ImageUpload from "./components/ImageUpload";
+import Home from "./components/Home";
+import Conversation from "./components/Conversation";
 
 const App = () => {
   return (
     <div>
-      <ImageUpload />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/upload' element={<ImageUpload />}/>
+          <Route path='/conversation' element={<Conversation />}/>
+        </Routes>
+      </Router>
     </div>
   );
 };
